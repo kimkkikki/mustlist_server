fb740c4f97e8a95e29059f713033ec1742197511
kimkkikki <donggeun.kim@questcompany.io>
2017-01-22 14:41:01 +0900

    add admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
+admin.site.register(models.Must)
+admin.site.register(models.User)
+admin.site.register(models.Pay)
+admin.site.register(models.MustCheck)
+admin.site.register(models.Score)
